b1fa2def17bf511bb4a05df2963337a0b09e339c
Gabriel Ahrens <gab.ahrens@gmail.com>
2022-04-05 16:48:31 +0200

    Botão para escutar música funcionando. Feita a função para escolher um objeto aleatório.

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,12 @@ const welcomeText = document.getElementById('instructionsText')
 const jogoMain = document.getElementById('question-container')
 const enviarButton = document.getElementById('submitbtn')
 const proximaButton = document.getElementById('nextbtn')
+const ouvirButton = document.getElementById('listenbtn')
 
 
-comecarButton.addEventListener("click", iniciarJogo)
+comecarButton.addEventListener("click", iniciarJogo);
+ouvirButton.addEventListener("click", escutarMusica);
+proximaButton.addEventListener("click", randomMusic);
 
 function iniciarJogo() { // vai dar inicio ao jogo, faz o botão 'Começar' desaparecer.
     comecarButton.classList.add('hide');
@@ -15,10 +18,32 @@ function iniciarJogo() { // vai dar inicio ao jogo, faz o botão 'Começar' desa
     proximaButton.classList.remove('hide');
 }
 
+function randomMusic() {
+    return music[Math.floor(Math.random()*music.length)];
+}
+
+
+function escutarMusica() {
+    var audio = new Audio("./assets/audio/hey-ya-outkast.mp3");
+    audio.play();
+}
+
+function nomeMusica() {
+}
+
+
+function nomeArtista() {
+}
+
+
+function reposta() {
+    
+}
+
 function proximaPergunta() {
     // Passa para a próxima pergunta
 }
 
-function resposta() {
-    // Resposta que o jogador escolher(ou escrever)
+function enviarResposta() {
+    // Resposta que o jogador escolher(ou escrever)?
 }
